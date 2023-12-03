4d568b76c1a615dc4eec110ee29e025fd61309e1
JaiKrishnasing <150617032+JaiKrishnasing@users.noreply.github.com>
2023-12-03 18:24:55 +0100

    Update checkout.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -48,7 +48,7 @@ function addItem() {
           &euro; ${storedProduct[i].productPrice.toFixed(2)},-
         </div>
         <div class="item-remove-icon">
-          <img src="/icons/icons8-close-30.png">
+          <img src="../icons/icons8-close-30.png">
         </div>
       </div>
     `;
